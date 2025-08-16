bf32de103aadf30a4b52ca29547f3ad1411290eb
Srushti <srushtibelegere96@gmail.com>
2025-08-16 22:50:06 +0530

    Update jenkins-install.sh

--- JenkinsServer/jenkins-install.sh
+++ JenkinsServer/jenkins-install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-sudo apt-get update
-yes | sudo apt install openjdk-11-jdk-headless
-echo "Waiting for 30 seconds before installing the jenkins package..."
-sleep 30
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+# For Ubuntu 22.04
+# Intsalling Java
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+sudo apt install openjdk-17-jdk -y
+java --version
+
+# Installing Jenkins
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-yes | sudo apt-get install jenkins
-sleep 30
-echo "Waiting for 30 seconds before installing the Terraform..."
-wget https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_linux_386.zip
-yes | sudo apt-get install unzip
-unzip 'terraform*.zip'
-sudo mv terraform /usr/local/bin/
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+
+
+
+# Installing AWS CLI
+#!/bin/bash
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
