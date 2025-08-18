aee9f22a1319fcc7fae54edd9398085f3e53c55e
Srushti <srushtibelegere96@gmail.com>
2025-08-18 10:20:28 +0530

    Update main.tf

--- eks-local/main.tf
+++ eks-local/main.tf
@@ -1,21 +1,53 @@
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr
+
+  azs = data.aws_availability_zones.azs.names
+
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = 1
+  }
+
+}
+
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "21.1.0"
+  source = "terraform-aws-modules/eks/aws"
 
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.28"
 
-  vpc_config = {
-    subnet_ids             = module.vpc.private_subnets
-    endpoint_public_access = true
-  }
+  cluster_endpoint_public_access = true
 
-  managed_node_groups = {
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
     nodes = {
-      desired_capacity = 2
-      min_capacity     = 1
-      max_capacity     = 3
-      instance_type    = "t2.small"
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_type = ["t2.small"]
     }
   }
 
